af87debbe786d0808bb17a156dda34057551bc42
Jennifer Prince <jennifergraceprince@gmail.com>
2020-08-30 05:03:35 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,16 +1,19 @@
-class Dog 
-  attr_accessor :name, :breed, :age 
-  Dogs = []
-  
+
+class Dog
+
+  attr_accessor :name, :breed, :age
+
+    @@all = []
+
   def initialize(name, breed, age)
-   @name = name
-   @breed = breed
-   @age = age
-   DOGS << self
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
   end
-  
-  def self.all 
-    DOGS 
+
+  def self.all
+      @@all
   end
-  
+
 end
